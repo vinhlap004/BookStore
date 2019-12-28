c79cf2c3d161ba791b2d232acd9acb894042069d
chuberacroi412 <37958877+chuberacroi412@users.noreply.github.com>
2019-12-28 16:12:26 +0700

    Giao diện và tương tác cho thêm/chỉnh sửa sản phẩm

---
+++ Bookstore/Bookstore/Dialog/AddProduct.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Bookstore.BUS;
+
+namespace Bookstore.Dialog
+{
+    /// <summary>
+    /// Interaction logic for AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        private int _id;
+        public AddProduct(int idForNewProduct)
+        {
+            _id = idForNewProduct;
+            InitializeComponent();
+        }
+
+        private void AddProduct_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int price, amount;
+            string type, author;
+            if(this.ProductName.Text == "" || this.Author.Text == "" || this.Price.Text == "" || this.Catalogries.Text == "" || this.Amount.Text == "" || this.Deliver.Text == "" || (this.Book_typeProduct.IsChecked == false && this.Stationery_typeProduct.IsChecked == false))
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin");
+            else
+            {
+                if (int.TryParse(this.Price.Text, out price) && int.TryParse(this.Amount.Text, out amount))
+                {
+                    type = this.Book_typeProduct.IsChecked == true ? "Book" : "Stationery";
+                    author = type == "Stationery" ? "-" : this.Author.Text;
+                    ManagerInventoryBus.addProduct(_id, type, this.ProductName.Text, price, amount, author, this.Deliver.Text, this.Catalogries.Text);
+
+                    DialogResult = true;
+                    this.Close();
+                }
+                else
+                    MessageBox.Show("Please type a number for Price and Amount, not a letter ");
+            }
+            
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
---
+++ Bookstore/Bookstore/Dialog/UpdateProduct.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Bookstore.BUS;
+
+namespace Bookstore.Dialog
+{
+    /// <summary>
+    /// Interaction logic for UpdateProduct.xaml
+    /// </summary>
+    public partial class UpdateProduct : Window
+    {
+        int _idProductSelected;
+        public UpdateProduct(int idProductSelected)
+        {
+            _idProductSelected = idProductSelected;
+
+            InitializeComponent();
+        }
+
+        private void EditProduct_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int price = -1, amount = -1;
+            if (!(this.Price.Text == "" || int.TryParse(this.Price.Text, out price)) && (this.Amount.Text == "" || int.TryParse(this.Amount.Text, out amount)))
+            {
+                MessageBox.Show("Please type a number for Price and Amount, not a letter");
+                return;
+            }
+
+            int.TryParse(this.Amount.Text, out amount);// ko hiu lam tai sao sai 
+            ManagerInventoryBus.updateProduct(_idProductSelected, this.ProductName.Text, price, amount, this.Author.Text, this.Deliver.Text, this.Catalogries.Text);
+
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
