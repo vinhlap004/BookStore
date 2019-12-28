1260e684bd1a1b38ad8a426c135ac668ab839287
chuberacroi412 <37958877+chuberacroi412@users.noreply.github.com>
2019-12-28 16:06:00 +0700

    Lớp phụ trợ cho việc hiển thị Bill dễ dàng

---
+++ Bookstore/Bookstore/BUS/BillInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.BUS
+{
+    class BillInfo
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public int Unit { set; get; }
+        public int Price { set; get; }
+        public int Cost { set; get; }
+    }
+}
