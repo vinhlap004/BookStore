1f63c359b44102d9aafd2728dd4aea0a4cf4cfcd
chuberacroi412 <37958877+chuberacroi412@users.noreply.github.com>
2019-12-28 16:19:52 +0700

    Giao diện và tương tác của Checkout và Quản lí kho

--- Bookstore/Bookstore/UserControls/Checkout_UserControl.xaml.cs
+++ Bookstore/Bookstore/UserControls/Checkout_UserControl.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bookstore;
+using Bookstore.BUS;
 
 namespace Bookstore.UserControls
 {
@@ -20,14 +22,193 @@ namespace Bookstore.UserControls
     /// </summary>
     public partial class Checkout_UserControl : UserControl
     {
+     
+        private List<Product> databaseSource = new List<Product>(); // lưu trữ nguyên bản tất cả sản phẩm từ csdl
+        private List<BillInfo> productOnBill = new List<BillInfo>();// danh sách sản phẩm hiển thị trên hóa đơn
+        private List<Product> listProduct = new List<Product>(); // danh sách sản phẩm hiển thị bên khung tìm kiếm
+        private int _subCost = 0;
+        private double _totalCost = 0;
+        private string _taxValue;
+        private string _billID;
+
+        //_1712872.Bookstore connectiondb; // remember to edit it
         public Checkout_UserControl()
         {
+            //connectiondb = new Bookstore();
+            databaseSource = CheckoutBus.getAllProduct();
+            _billID = CheckoutBus.generateBillID();
+
+
             InitializeComponent();
+            _taxValue = this.TaxTextBlock.Text;
+            BillIDTextBlock.Text = "Bill ID : " + _billID;
+            this.DateTextBlock.Text = "Date: " + DateTime.Today.ToShortDateString();
         }
+
+        //*************************************************************************************************************************
+        //*                                                    CÁC HÀM PHỤ TRỢ                                                    *
+        //*************************************************************************************************************************
+
+        //------------------> Các hàm dọn dẹp giao diện và data behind <---------------------
+        private void resetUI()
+        {
+            BillIDTextBlock.Text = "Bill ID : " + _billID;
+            this.Bill_ListView.ItemsSource = null;
+            this.Customer_ListView.ItemsSource = null;
+        }
+
+        private void clearCostTextBlock()
+        {
+            this.SubCostTextBlock.Text = "";
+            this.TotalCostTextBlock.Text = "";
+        }
+
+        private void clearBindingList()
+        {
+            productOnBill.Clear();
+            listProduct.Clear();
+        }
+        //------------------> Hết các hàm dọn dẹp giao diện và databehind <---------------------
+
+        //------------------> Các hàm tính giá tiền của cái bill <-----------------------
+        private void updateCostTextBlock(int priceOfProduct)
+        {
+            if (this.TaxTextBlock.Text != this._taxValue)
+                this._taxValue = this.TaxTextBlock.Text;
+
+            this._subCost = CheckoutBus.updateSubCost(this._subCost, priceOfProduct);
+            this._totalCost += CheckoutBus.updateTotalCost(this._subCost, this._taxValue);
+
+            this.SubCostTextBlock.Text = this._subCost.ToString();
+            this.TotalCostTextBlock.Text = this._totalCost.ToString();
+        }
+        //------------------> Hết các hàm tính giá tiền của cái bill <-----------------------
+
+
+       
+   
+//===============================================================================================================================================
+
+
+        //*******************************************************************************************************
+        //*                                      CÁC HÀM XỬ LÍ SỰ KIỆN                                          *
+        //*******************************************************************************************************
 
         private void Customer_ListView_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.PopupEnabled.IsChecked = false;
+
+            listProduct = CheckoutBus.search(databaseSource, this.searchTextBlock.Text);
+            this.Customer_ListView.ItemsSource = listProduct;
+        }
+
+        private void AddProduct_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Customer_ListView.SelectedItem == null)
+                MessageBox.Show("Phải chọn sản phẩm mới nhấn được người ơi");
+            else
+            {
+                var itemSelected = this.Customer_ListView.SelectedItem as Product;
+
+                var productCheck = (from item in databaseSource where item.ID == itemSelected.ID select item).ToList();
+
+                if(productCheck[0].Amount < 1)
+                {
+                    MessageBox.Show("Hiện sản phẩm đã hết hàng");
+                    return;
+                }
+
+                foreach (var item in productOnBill)
+                {
+                    if (item.ID == itemSelected.ID)
+                    {
+                        if(productCheck[0].Amount - item.Unit - 1 < 0)
+                        {
+                            MessageBox.Show("Hiện sản phẩm đã hết hàng");
+                            return;
+                        }
+
+                        item.Unit++;
+                        item.Cost += item.Price;
+                        this.Bill_ListView.ItemsSource = null;
+                        this.Bill_ListView.ItemsSource = productOnBill;                     
+                        updateCostTextBlock(item.Price);
+                        return;
+                    }
+                }
+
+                productOnBill.Add(new BillInfo() { ID = itemSelected.ID, Name = itemSelected.Name, Price = itemSelected.Price, Unit = 1, Cost = itemSelected.Price });
+                updateCostTextBlock(itemSelected.Price);
+
+                this.Bill_ListView.ItemsSource = null;
+                this.Bill_ListView.ItemsSource = productOnBill;
+            }
+        }
+
+        private void clearBill_Button_Click(object sender, RoutedEventArgs e)
+        {
+            resetUI();
+            clearBindingList();
+            clearCostTextBlock();
+        }
+
+        private void Checkout_Button_Click(object sender, RoutedEventArgs e)
+        {
+            updateCostTextBlock(0); // chạy lần cuối để kiểm tra xem tax có bị thay đổi ko, nếu có => tính lại theo tax mới
+
+            CheckoutBus.addBill(_billID, _totalCost);
+
+            // cập nhật csdl
+            CheckoutBus.updateAmount(productOnBill);
+            CheckoutBus.addTrade(productOnBill, _billID);
+
+            _billID = (int.Parse(_billID) + 1).ToString();
+
+            //hoàn tất cập nhật thông tin, xử lí giao diện, dọn dẹp chuẩn bị cho cái bill tiếp theo
+            resetUI();
+            clearBindingList();
+            clearCostTextBlock();
+
+        }
+
+        private void FilterOKButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<Product> temp = new List<Product>();
+            temp.AddRange(listProduct);
+            if (this.Customer_ListView.ItemsSource == null)
+                MessageBox.Show("Không có gì để mà lọc đâu người hỡi");
+            else
+            {
+                string[] tranformer = { "All Types", "Book", "Stationery" };
+
+                var typeFilterSelected = tranformer[this.TypeFilterComboBox.SelectedIndex == -1 ? 0 : this.TypeFilterComboBox.SelectedIndex];
+
+                if (typeFilterSelected != "All Types")
+                    temp = CheckoutBus.filterType(temp, typeFilterSelected);
+
+                if (this.AuthorFilterCheckBox.IsChecked == true && this.AuthorFilterTextBlock.Text != "")
+                    temp = CheckoutBus.filterAuthor(temp, this.AuthorFilterTextBlock.Text);
+
+                if (this.DeliverFilterCheckBox.IsChecked == true && this.DeliverFilterTextBlock.Text != "")
+                    temp = CheckoutBus.filterDeliver(temp, this.DeliverFilterTextBlock.Text);
+
+                if (this.CatalogriesFilterCheckBox.IsChecked == true && this.CatalogriesFliterTextBlock.Text != "")
+                    temp = CheckoutBus.filterCatalogries(temp, this.CatalogriesFliterTextBlock.Text);
+
+                this.Customer_ListView.ItemsSource = null;
+                this.Customer_ListView.ItemsSource = temp;
+            }
+        }
+
+        private void PopupEnabled_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Customer_ListView.ItemsSource = null;
+            this.Customer_ListView.ItemsSource = listProduct;
         }
     }
 }
--- Bookstore/Bookstore/UserControls/Manager_Inventory_UserControl.xaml.cs
+++ Bookstore/Bookstore/UserControls/Manager_Inventory_UserControl.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bookstore;
+using Bookstore.Dialog;
+using Bookstore.BUS;
 
 namespace Bookstore.UserControls
 {
@@ -20,10 +23,133 @@ namespace Bookstore.UserControls
     /// </summary>
     public partial class Manager_Inventory_UserControl : UserControl
     {
+
+        //private _1712872.Bookstore connectiondb;
+        private List<Product> _databaseSource = new List<Product>();
+        private List<Product> _displayProductList = new List<Product>();
+
         public Manager_Inventory_UserControl()
         {
+
+            _databaseSource = ManagerInventoryBus.getAllProduct();
+            
             InitializeComponent();
+            Customer_ListView.ItemsSource = _databaseSource;
         }
 
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Customer_ListView.ItemsSource = null;
+
+            if (this.SearchTextBox.Text == "")
+                Customer_ListView.ItemsSource = _databaseSource;
+
+            _displayProductList = ManagerInventoryBus.search(_databaseSource, this.SearchTextBox.Text);
+            Customer_ListView.ItemsSource = _displayProductList;
+        }
+
+        private void FilterOkButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] tranform = { "All Types", "Book", "Stationery" };
+
+            if(this._displayProductList.Count == 0)
+                this._displayProductList.AddRange(_databaseSource);
+
+            string typeFilterSelected = tranform[(this.ComboboxFilter.SelectedIndex == -1 ? 0 : this.ComboboxFilter.SelectedIndex)];
+
+            if (typeFilterSelected != "All Types")
+                _displayProductList = ManagerInventoryBus.filterType(_displayProductList, typeFilterSelected);
+
+            if (this.AuthorCheckboxFilter.IsChecked == true && this.AuthorTextBoxFilter.Text != "")
+                _displayProductList = ManagerInventoryBus.filterAuthor(_displayProductList, this.AuthorTextBoxFilter.Text);
+
+            if (this.DeliverCheckboxFilter.IsChecked == true && this.DeliverTextBoxFilter.Text != "")
+                _displayProductList = ManagerInventoryBus.filterDeliver(_displayProductList, this.DeliverTextBoxFilter.Text);
+
+            if (this.CataloriesCheckboxFilter.IsChecked == true && this.CataloriesTextBoxFilter.Text != "")
+                _displayProductList = ManagerInventoryBus.filterCatalogries(_displayProductList, this.CataloriesTextBoxFilter.Text);
+
+            this.Customer_ListView.ItemsSource = null;
+            this.Customer_ListView.ItemsSource = _displayProductList;
+
+        }
+
+        private void PopupEnabled_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Customer_ListView.ItemsSource = null;
+            this.Customer_ListView.ItemsSource = _databaseSource;
+        }
+
+        private void AddProduct_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int nextProductID = ManagerInventoryBus.generateProductID();
+
+            var addProductScreen = new AddProduct(nextProductID);
+
+            if(addProductScreen.ShowDialog() == true)
+            {
+                _databaseSource = ManagerInventoryBus.getAllProduct();
+                this.Customer_ListView.ItemsSource = null;
+                this.Customer_ListView.ItemsSource = _databaseSource;
+            }
+        }
+
+        private void UpdateProduct_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Customer_ListView.SelectedItem == null)
+                MessageBox.Show("Bạn phải chọn sản phẩm trước");
+            else
+            {
+                var editProductScreen = new UpdateProduct((this.Customer_ListView.SelectedItem as Product).ID);
+
+                if (editProductScreen.ShowDialog() == true)
+                {
+
+                    //connectiondb = new Bookstore();
+                    _databaseSource = ManagerInventoryBus.getAllProduct();
+                    this.Customer_ListView.ItemsSource = null;
+                    this.Customer_ListView.ItemsSource = _databaseSource;
+                }
+            }
+        }
+
+        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Customer_ListView.SelectedItem == null)
+                MessageBox.Show("Bạn phải chọn sản phẩm trước");
+            else
+            {
+
+                var selectedItem = this.Customer_ListView.SelectedItem as Product;
+                ManagerInventoryBus.deleteProduct(selectedItem.ID);
+
+                _databaseSource = ManagerInventoryBus.getAllProduct();
+                this.Customer_ListView.ItemsSource = null;
+                this.Customer_ListView.ItemsSource = _databaseSource;
+            }
+        }
+
+        private void EditMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var editProductScreen = new UpdateProduct((this.Customer_ListView.SelectedItem as Product).ID);
+
+            if (editProductScreen.ShowDialog() == true)
+            {
+
+                //connectiondb = new Bookstore();
+                _databaseSource = ManagerInventoryBus.getAllProduct();
+                this.Customer_ListView.ItemsSource = null;
+                this.Customer_ListView.ItemsSource = _databaseSource;
+            }
+        }
+
+        private void DeleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ManagerInventoryBus.deleteProduct((this.Customer_ListView.SelectedItem as Product).ID);
+
+            _databaseSource = ManagerInventoryBus.getAllProduct();
+            this.Customer_ListView.ItemsSource = null;
+            this.Customer_ListView.ItemsSource = _databaseSource;
+        }
     }
 }
